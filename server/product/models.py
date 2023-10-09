51d3a17183821b6fa9d8316395c050ae6304c4fb
samuel tefera <samueltefer23@gmail.com>
2023-10-09 12:16:36 +0300

    Update docs

--- server/product/models.py
+++ server/product/models.py
@@ -4,6 +4,18 @@ from supplier.models import Supplier
 
 
 class Product(TimeStamp):
+    """
+    A model representing a product in the inventory.
+
+    Attributes:
+    - name (str): The name of the product.
+    - brand (str): The brand of the product.
+    - unit (Unit): The unit of measurement for the product.
+    - supplier (Supplier): The supplier of the product.
+
+    Methods:
+    - __str__(): Returns the name of the product as a string.
+    """
     name = models.CharField(max_length=63)
     brand = models.CharField(max_length=50)
     unit = models.ForeignKey(to=Unit, on_delete=models.SET_NULL, null=True)
--- server/product/serializers.py
+++ server/product/serializers.py
@@ -4,6 +4,11 @@ from product.models import Product
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Product model.
+
+    Includes the `unit_title` field, which is a read-only field that represents the title of the unit associated with the product.
+    """
     unit_title = serializers.CharField(source='unit.title', required=False)
 
     class Meta:
